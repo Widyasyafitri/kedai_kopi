1363cc230acb5508c28daafd1dd203094628bc98
Widyasyafitri <widiasafitri9917@gmail.com>
2025-06-18 10:20:54 +0700

    Menambahkan ikon keranjang belanja

--- js/script.js
+++ js/script.js
@@ -1,12 +1,10 @@
-// toggle class active
-// toggle class active untuk hamburger menu
+// Toggle class active untuk hamburger menu
 const navbarNav = document.querySelector(".navbar-nav");
-// ketika hamburger menu di klik
 document.querySelector("#hamburger-menu").onclick = () => {
   navbarNav.classList.toggle("active");
 };
 
-// toggle class active untuk search form
+// Toggle class active untuk search form
 const searchForm = document.querySelector(".search-form");
 const searchBox = document.querySelector("#search-box");
 
@@ -16,21 +14,28 @@ document.querySelector("#search-button").onclick = (e) => {
   e.preventDefault();
 };
 
-// klik di luar elemen
-const hm = document.querySelector("#hamburger-menu");
-const sb = document.querySelector("#search-button");
+// Toggle class active untuk shopping cart
+const shoppingCart = document.querySelector(".shopping-cart");
+document.querySelector("#shopping-cart-button").onclick = (e) => {
+  shoppingCart.classList.toggle("active");
+  e.preventDefault();
+};
 
-// klik di luar sidebar untuk mnghilangkan nav
-const hamburger = document.querySelector("#hamburger-menu");
-
+// Klik di luar elemen untuk menutup
 document.addEventListener("click", function (e) {
-  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
-    if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
-      navbarNav.classList.remove("active");
-    }
+  const hm = document.querySelector("#hamburger-menu");
+  const sb = document.querySelector("#search-button");
+  const sc = document.querySelector("#shopping-cart-button");
+
+  if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
   }
 
   if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
     searchForm.classList.remove("active");
+  }
+
+  if (!sc.contains(e.target) && !shoppingCart.contains(e.target)) {
+    shoppingCart.classList.remove("active");
   }
 });
