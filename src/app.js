3b561ea53788ed6df4b005932922ee761a3946c6
Widyasyafitri <widiasafitri9917@gmail.com>
2025-06-25 15:59:41 +0700

    Integrasi feather sprite dan dinding harga dengan Alpine.js

--- src/app.js
+++ src/app.js
@@ -21,3 +21,12 @@ document.addEventListener("alpine:init", () => {
     },
   });
 });
+
+// konversi ke rupiah
+const rupiah = (number) => {
+  return new Intl.NumberFormat("id-ID", {
+    style: "currency",
+    currency: "IDR",
+    minimumFractionDigits: 0,
+  }).format(number);
+};
